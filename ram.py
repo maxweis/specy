a6c22a9160c16140cd2b4bce1de76c627b8c559f
Max Weis <maxrobweis@gmail.com>
2015-01-17 00:11:31 -0600

    inital git specy commit

---
+++ ram.py
@@ -0,0 +1,44 @@
+#! /bin/python
+
+
+#specy:  an Arch Linux system command line description tool
+   
+#Copyright (C) 2014  Max Weis
+
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+
+import os
+import sys
+
+#Get readable free memory output
+memory = (os.popen("free -h")).read()
+
+memory = memory.split('\n')
+
+used_memory = memory[1].split('      ')
+total_memory = memory[1].split('      ')
+
+used_memory = used_memory[2][2:]
+
+total_memory = total_memory[1][4:]
+if (len(sys.argv) < 2):
+    print("Usage:  " + sys.argv[0] + " [-t] [-u]")
+
+elif sys.argv[1] == '-t' or sys.argv[1] == "--total":
+    print(total_memory)
+
+elif sys.argv[1] == '-u' or sys.argv[1] == "--used":
+    print(used_memory)
+
+else:
+    print("Usage:  " + sys.argv[0] + "[-t] [f]")
